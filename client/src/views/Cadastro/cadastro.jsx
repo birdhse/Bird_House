46ff92fb329cd22bfeada30f991163b81e2e2d46
SARAH MONTEIRO DUQUE <s.duque@labcetec.net>
2024-06-03 17:28:16 -0300

    aaaaaaaa

---
+++ client/src/views/Cadastro/cadastro.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+function Cadastros() {
+    return (
+        <div>
+            <div class="container">
+                <div id="navegacao">
+                    <div id="area_menu">
+                        <h1 id="logo">Bird House</h1>
+
+                        <input type="text" id="txtBusca" placeholder="Buscar..." />
+                        <label id="data">23/04/2024</label>
+                    </div>
+                </div>
+            </div>
+
+            <div id="menu_lateral">
+                <ul>
+                <li><a href="telaPrincipal.html">Geral</a></li>
+                            <li><a href="link2.htm">Mapa</a></li>
+                            <li><a href="reserva.html">Reservas</a></li>
+                            <li class="link_ativo"><a href="link4.htm">Cadastros</a></li>
+                            <li><a href="link5.htm">Transações</a></li>
+                        </ul>
+            </div>
+
+                    <div class="main">
+                        <div class="breadcrumb">
+                            Geral / Cadastros
+                    </div>
+                        <div class="content">
+                            <h2>Amanda Xavier</h2>
+                                <div class="tabs">
+                                    <button class="tab active" onclick="showTab('info')">Informações do hóspede</button>
+                                    <button class="tab" onclick="showTab('history')">Histórico de estadias</button>
+                                </div>
+                        </div>
+                        <div class="tab-content" id="info">
+                            <div class="profile-pic">
+                                <img src="avatar.png" alt="Foto de perfil">
+                                <button>Selecionar uma foto (Proporção: 500 x 500)</button>
+                            </div>
+                        </div>
+                        <form>
+                            <label for="nome">Nome completo</label>
+                            <input type="text" id="nome" value="Amanda Xavier">
+
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" value="amanda@gmail.com">
+
+                            <label for="cpf">CPF</label>
+                            <input type="text" id="cpf" value="000000000-00">
+
+                            <label for="nascimento">Nascimento</label>
+                            <input type="date" id="nascimento" value="2003-03-30">
+
+                            <label for="sexo">Sexo</label>
+                            <input type="text" id="sexo" value="Feminino">
+
+                            <label for="contato">Contato</label>
+                            <input type="text" id="contato" value="(00) 000000000">
+                        </form>
+                        </div>
+                             <div class="tab-content" id="history" style="display:none;">
+                                <p>Histórico de estadias não disponível.</p>
+                            </div>
+                        </div>
+                </div>
+                                                  
+
+
+ )
+}
+
+export default Cadastros
+
+<script src="js3/js3.js"></script>
+<script src="js3/js3.js"></script>
