47aecbc2d02b81ce0ebb5df72612a6ca0ec3ea5f
MATHEUS SARTI BRUNELLI <m.brunelli@labcetec.net>
2024-09-23 15:02:29 -0300

    Atualizando

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import './css/Geral.css';
 import './css/Relatorio.css';
 import './css/Mapa.css';
 import './css/Cadastro.css';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ import Geral from './views/Geral';
 import Reservas from './views/Reserva';
 import EditarReserva from './views/EditarReserva';
 import Cadastros from './views/Cadastro';
+
 
 function App() {
   return (
@@ -20,7 +22,7 @@ function App() {
         <Route path='/cadastros' element={<Cadastros />} />
         <Route path='/editreserva' element={<EditarReserva />} />
         <Route path='/reservas' element={<Reservas />} />
-        
+       
       </Routes>
     </BrowserRouter>
   );
