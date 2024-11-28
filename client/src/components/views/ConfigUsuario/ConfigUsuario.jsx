1796644b48e205ba8a7983af39f179d5a4f9c213
MATHEUS SARTI BRUNELLI <m.brunelli@labcetec.net>
2024-11-28 15:12:58 -0300

    ATIVO/INATIVO

--- client/src/components/views/ConfigUsuario/ConfigUsuario.jsx
+++ client/src/components/views/ConfigUsuario/ConfigUsuario.jsx
@@ -1,68 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from '../../layout/menu';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
 import styles from './Config.module.css';
 
 function ConfigUsuario() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div>
       <Menu /> 
-      
-    <div className="container mt-5 ">
 
-      <h1 className='text-center mt-3'>Configuração do Usuário</h1>
-      
-        <div className="content">
-         
-            
+      <div className="container mt-5 ">
+        <h1 className='text-center mt-3'>Configuração do Usuário</h1>
         
-            
+        <div className="content">
+          <h1>Amanda Xavier</h1>
 
-            <h1>Amanda Xavier</h1>
+          <form>
+            <div className="mb-4 ">
+              <label htmlFor="name" className="form-label">Nome Completo</label>
+              <input type="text" id="name" className="form-control w-1 p-2" defaultValue="Amanda Xavier" />
+            </div>
 
-            <form>
-              <div className="mb-4 ">
-                <label htmlFor="name" className="form-label">Nome Completo</label>
-                <input type="text" id="name" className="form-control w-1 p-2" defaultValue="Amanda Xavier" />
-              </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="form-label">E-mail</label>
+              <input type="email" id="email" className="form-control w-1 p-2" defaultValue="amanda@gmail.com" />
+            </div>
 
-              <div className="mb-4">
-                <label htmlFor="email" className="form-label">E-mail</label>
-                <input type="email" id="email" className="form-control w-1 p-2" defaultValue="amanda@gmail.com" />
-              </div>
+            <div className="mb-4">
+              <label htmlFor="role" className="form-label">Função</label>
+              <select id="relatorio" className="form-select w-1 p-2">
+                <option value="1">Administrador</option>
+                <option value="2">Funcionário Geral</option>
+              </select>
+            </div>
 
-              <div className="mb-4">
-                <label htmlFor="role" className="form-label">Função</label>
-                <select id="relatorio" className="form-select w-1 p-2">
-                   <option value="1">Admnistrador</option>
-                   <option value="2">Funcionário Geral</option>
-                </select>
-              </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="form-label">Senha</label>
+              <input type="password" id="password" className="form-control w-1 p-2" placeholder="" />
+            </div>
 
-              <div className="mb-4">
-                <label htmlFor="password" className="form-label">Senha</label>
-                <input type="password" id="password" className="form-control w-1 p-2" placeholder="" />
-              </div>
+            <div className="form-check mb-3 d-flex align-items-center gap-2">
+              <input 
+                type="checkbox" 
+                id="active" 
+                className="form-check-input" 
+                checked={isActive} 
+                onChange={handleCheckboxChange} 
+              />
+              <label htmlFor="active" className="form-check-label">
+                {isActive ? "Ativo" : "Inativo"}
+              </label>
+            </div>
 
-              <div className="form-check mb-3">
-                <input type="checkbox" id="active" className="form-check-input"/>
-                <label htmlFor="active" className="form-check-label w-1">Ativo</label>
-              </div>
-
-              <div className="d-flex align-items-center gap-2">
-                <button type="button" className="btn btn-info text-white" onClick={() => alert("Função para alterar a senha!")}>
-                  Alterar
-                </button>
-                <a href="#" className="text-primary" onClick={(e) => { e.preventDefault(); alert("Para ajuda entre em contato através do e-mail: matheus.brunelli@aluno.senai.br ou entre em contato com seu admnistrador") }}>
-                  Ajuda
-                </a>
-              </div>
-            </form>
-          </div>
+            <div className="d-flex align-items-center gap-2">
+              <button type="button" className="btn btn-info text-white" onClick={() => alert("Função para alterar a senha!")}>
+                Alterar
+              </button>
+              <a href="#" className="text-primary" onClick={(e) => { e.preventDefault(); alert("Para ajuda entre em contato através do e-mail: matheus.brunelli@aluno.senai.br ou entre em contato com seu administrador") }}>
+                Ajuda
+              </a>
+            </div>
+          </form>
         </div>
       </div>
-    
+    </div>
   );
 }
 
